6e300f3a3f44861e12c087d37f602206249b6147
Alexis Guzman <alexis12989x@gmail.com>
2021-08-03 15:12:30 -0500

    feat: set up custom app component with theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
+import { ThemeProvider } from 'next-themes';
+
 import '../styles/globals.scss';
 
+import config from '../config';
+
+const { theme } = config;
+
 const MyApp = ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
+	return (
+		<ThemeProvider
+			defaultTheme={theme}
+			enableSystem={false}
+			themes={['leaf', 'snow', 'noir']}
+		>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 };
 
 export default MyApp;
