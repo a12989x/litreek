8db15f8ccf6e2c1aaa7f48dd5633b774353f51bd
Alexis Guzman <alexis12989x@gmail.com>
2021-08-03 18:37:40 -0500

    feat(seo): set up next-seo config js file and import default seo component

---
+++ next-seo.config.js
@@ -0,0 +1,31 @@
+import config from './config';
+
+const { name } = config;
+
+const SEO = {
+	title: `${name} - Litreek`,
+	description: `Hi, I'm ${name} and this is a list of all my link neede to know me. Visit my links and let's chat`,
+	canonical: 'https://litreek.vercel.app',
+	openGraph: {
+		type: 'website',
+		locale: 'en_IE',
+		url: 'https://litreek.vercel.app',
+		title: `${name} - Litreek`,
+		description: `Hi, I'm ${name} and this is a list of all my link neede to know me. Visit my links and let's chat`,
+		images: [
+			{
+				url: 'https://fastfeedback.io/og.png',
+				alt: `${name} - Litreek`,
+				width: 1280,
+				height: 720,
+			},
+		],
+	},
+	twitter: {
+		handle: '@__codax__',
+		site: '@__codax__',
+		cardType: 'summary_large_image',
+	},
+};
+
+export default SEO;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import { ThemeProvider } from 'next-themes';
+import { DefaultSeo } from 'next-seo';
 
 import '../styles/globals.scss';
 
 import config from '../config';
+import SEO from '../next-seo.config';
 
 const { theme } = config;
 
@@ -13,6 +15,7 @@ const MyApp = ({ Component, pageProps }) => {
 			enableSystem={false}
 			themes={['leaf', 'snow', 'noir']}
 		>
+			<DefaultSeo {...SEO} />
 			<Component {...pageProps} />
 		</ThemeProvider>
 	);
