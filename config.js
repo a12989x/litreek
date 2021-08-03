3231efa2cce61f489db07bced44f39555199cda9
Alexis Guzman <alexis12989x@gmail.com>
2021-08-03 18:39:38 -0500

    feat: set up config js file

---
+++ config.js
@@ -0,0 +1,37 @@
+const config = {
+	name: 'Alexis Guzman',
+	subtle: 'Front-end web developer',
+	avatar: 'https://avatars.githubusercontent.com/u/48135533?v=4',
+	theme: 'noir',
+	links: [
+		{ header: 'Project' },
+		{
+			title: '💻 Repository',
+			url: 'https://github.com/a12989x',
+			backgroundColor: '',
+		},
+		{ header: 'Me' },
+		{
+			title: '🌐 Website',
+			url: 'https://codaxx.ml/',
+			backgroundColor: '',
+		},
+		{
+			title: '💻 GitHub',
+			url: 'https://github.com/a12989x/',
+			backgroundColor: '',
+		},
+		{
+			title: '📺 YouTube',
+			url: 'https://www.youtube.com/channel/UCMY0GhV1HuX4XdbgalC77VQ',
+			backgroundColor: '',
+		},
+		{
+			title: '📃 CV',
+			url: 'http://codax.leancv.com/',
+			backgroundColor: '',
+		},
+	],
+};
+
+export default config;
