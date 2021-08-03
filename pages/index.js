314689f4aa24a757acb27fcd9863462bff40f4f2
Alexis Guzman <alexis12989x@gmail.com>
2021-08-03 15:13:03 -0500

    feat(page): set up home page component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
-const index = () => {
+import Image from 'next/image';
+
+import Header from '../components/Header';
+import Link from '../components/Link';
+
+import config from '../config';
+
+const { name, subtle, avatar, links } = config;
+
+const Home = () => {
 	return (
-		<div>
-			<p>Home Page Component</p>
-		</div>
+		<main>
+			<Image src={avatar} alt={name} height='100' width='100' />
+
+			<h1>{name}</h1>
+			{subtle && <h2 className='subtle'>{subtle}</h2>}
+
+			<ul>
+				{links.map((link, index) => (
+					<li key={index}>
+						{link.header ? (
+							<Header {...link} />
+						) : (
+							<Link {...link} />
+						)}
+					</li>
+				))}
+			</ul>
+		</main>
 	);
 };
 
-export default index;
+export default Home;
