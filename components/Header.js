1dc29019f8c671652901d80b15406e07ce9be6b3
Alexis Guzman <codingcodax@gmail.com>
2023-03-04 20:01:59 -0600

    remove prev version

--- components/Header.js
+++
@@ -1,5 +0,0 @@
-const Header = ({ header }) => {
-	return <h2 className='litreek_header'>{header}</h2>;
-};
-
-export default Header;
--- next.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	reactStrictMode: true,
-	images: { domains: ['avatars.githubusercontent.com'] },
-};
