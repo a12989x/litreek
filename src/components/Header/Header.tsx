885a51cd2dd0052e8cdf6f527c88ba0bc718aae6
Alexis Guzman <codingcodax@gmail.com>
2023-03-05 14:10:41 -0600

    feat(header): replace link with nav link component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,36 @@
+import { Link } from '~/components/ui';
+
+import NavLink from './NavLink';
+
 const Header = () => {
   return (
-    <div>
-      <p>Header</p>
-    </div>
+    <header className='sticky top-0'>
+      <nav className='mx-auto max-w-screen-xl px-8 py-4'>
+        <ul className='flex h-10 items-center gap-6'>
+          <li>
+            <Link href='/'>Logo</Link>
+          </li>
+          <NavLink href='/register'>Get Started</NavLink>
+          <NavLink href='#features'>Features</NavLink>
+          <NavLink href='#pricing'>Pricing</NavLink>
+          <NavLink href='#faq'>FAQ</NavLink>
+
+          <li className='!ml-auto'>
+            <Link href='/login' className='hover:bg-gray-200' intent='nav'>
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link href='/signup'>
+              Sign up
+              <span className='font-normal italic'>
+                &nbsp; &mdash; it&apos;s free
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
