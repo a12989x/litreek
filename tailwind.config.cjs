2989ecd1662a7fd8065eec869c0b34b5db680d14
Alexis Guzman <codingcodax@gmail.com>
2023-03-05 09:28:49 -0600

    chore(tailwind): add cta bg keyframe and animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,16 @@ const config = {
       fontFamily: {
         sans: ['var(--font-sans)', ...fontFamily.sans],
       },
+      keyframes: {
+        'cta-bg': {
+          '0%': { 'background-position': '0 0' },
+          '50%': { 'background-position': '100%' },
+          '100%': { 'background-position': '0 0' },
+        },
+      },
+      animation: {
+        'cta-bg': 'cta-bg 2s ease-in-out infinite',
+      },
     },
   },
   plugins: [],
