ccca0d229432730996c48586f4cb131e1f1787db
Alexis Guzman <codingcodax@gmail.com>
2023-03-05 14:49:44 -0600

    feat(footer): use footer link component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,32 @@
+import { Link } from '~/components/ui';
+
+import FooterLink from './FooterLink';
+
 const Footer = () => {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <footer className='mx-auto max-w-screen-xl px-8 py-20'>
+      <nav className='flex justify-between'>
+        <ul className='flex gap-4'>
+          <li>
+            <Link href='/'>Logo</Link>
+          </li>
+          <FooterLink href='/help'>Help Center</FooterLink>
+          <FooterLink href='/terms'>Terms of Use</FooterLink>
+          <FooterLink href='/privacy'>Privacy Policy</FooterLink>
+        </ul>
+
+        <p>
+          Litreek is a{' '}
+          <Link
+            href='https://dub.sh/codingcodax'
+            className='bg-gradient-to-r from-pink-500 to-purple-500 bg-clip-text font-medium text-transparent'
+          >
+            @codingcodax
+          </Link>{' '}
+          creation
+        </p>
+      </nav>
+    </footer>
   );
 };
 
