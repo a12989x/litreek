fec96ffe24ff8911631d3fcc9205029ff8597c60
Alexis Guzman <codingcodax@gmail.com>
2023-03-05 09:29:36 -0600

    feat(home): setup hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from 'next';
 import Link from 'next/link';
+import { Button } from '~/components/ui';
 
 const Home: NextPage = () => {
   return (
@@ -31,21 +32,37 @@ const Home: NextPage = () => {
       </header>
 
       <div className='mt-16 px-8'>
-        <main className=' text-center'>
-          <h1 className='mb-4 text-6xl font-bold'>
+        <main className='flex flex-col items-center'>
+          <h1 className='mb-4 text-center text-6xl font-bold'>
             One link to <br /> multiple pages
           </h1>
           <h2 className='mb-8 text-2xl'>
             Build a beautiful site for all your links and get proper analytics
             of your visitors.
           </h2>
-          <form className='mb-16'>
-            <input type='text' placeholder='litreek.link/yourname' />
-            <button type='submit'>Claim &mdash; it&apos;s free</button>
+          <form className='relative mb-16 flex items-center justify-center overflow-hidden rounded-lg font-medium'>
+            <div className='ml-0.5 flex h-[44px] items-center justify-center rounded-l-lg bg-white pl-6'>
+              <span>litreek.link/</span>
+            </div>
+            <input
+              type='text'
+              className='peer h-[44px] w-52 pr-6 text-purple-500 outline-none placeholder:text-gray-400'
+              placeholder='yourname'
+            />
+            <div className='peer-focus:animate-cta-bg peer-hover:animate-cta-bg absolute inset-0 -z-10 bg-gradient-to-r from-pink-500 via-indigo-500 to-purple-500 bg-[length:165%]' />
+            <Button
+              href='/signup'
+              size='large'
+              type='submit'
+              intent='cta'
+              className='bg-none'
+            >
+              Claim &mdash; it&apos;s free
+            </Button>
           </form>
         </main>
 
-        <div className='h-[600px] w-full bg-gray-100' />
+        <div className='h-[600px] w-full rounded-lg bg-gray-100' />
       </div>
     </div>
   );
