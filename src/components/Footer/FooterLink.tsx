b1b3b5b8ca835d91689cc7ba60ecbfa823ddc729
Alexis Guzman <codingcodax@gmail.com>
2023-03-05 14:48:39 -0600

    feat(footer): setup footer link

--- src/components/Footer/FooterLink.tsx
+++ src/components/Footer/FooterLink.tsx
@@ -1,8 +1,17 @@
-const FooterLink = () => {
+import { Link } from '~/components/ui';
+
+type Props = {
+  href: string;
+  children: React.ReactNode;
+};
+
+const FooterLink = ({ href, children }: Props) => {
   return (
-    <div>
-      <p>Footer Link</p>
-    </div>
+    <li>
+      <Link href={href} intent='footer'>
+        {children}
+      </Link>
+    </li>
   );
 };
 
