9d1bf24e69f0f998eada8ea7a86bcdbdadfb35f5
Alexis Guzman <codingcodax@gmail.com>
2023-03-04 20:18:31 -0600

    chore(prettier): add config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,34 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [
+    require.resolve('prettier-plugin-tailwindcss'),
+    require.resolve('@ianvs/prettier-plugin-sort-imports'),
+  ],
+  endOfLine: 'lf',
+  jsxSingleQuote: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  importOrder: [
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^types$',
+    '^@/types/(.*)$',
+    '^@/lib/(.*)$',
+    '^@/components/(.*)$',
+    '^@/styles/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
 };
 
 module.exports = config;
