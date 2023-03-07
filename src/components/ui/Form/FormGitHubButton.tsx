c0326fdae4904e1a9d1e0a93b964e7b28012866b
Alexis Guzman <codingcodax@gmail.com>
2023-03-07 17:56:39 -0600

    feat(form): create and setup form github button

---
+++ src/components/ui/Form/FormGitHubButton.tsx
@@ -0,0 +1,22 @@
+import { signIn } from 'next-auth/react';
+import { GitHubIcon } from '~/components/icons';
+import { Button } from '~/components/ui';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const FormGitHubButton = ({ children }: Props) => {
+  return (
+    <Button
+      type='button'
+      onClick={() => signIn('github', { callbackUrl: '/' })}
+      intent='ghost'
+      className='w-full space-x-2'
+    >
+      <GitHubIcon className='h-4 w-4' /> <span>{children}</span>
+    </Button>
+  );
+};
+
+export default FormGitHubButton;
