0926415daf10b08ba3b0faf82cdee09f4a36263a
Alexis Guzman <codingcodax@gmail.com>
2023-03-07 13:33:49 -0600

    feat(header): add user select none and pointer none

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import NavLink from './NavLink';
 const Header = () => {
   return (
     <header className='sticky top-0 z-20'>
-      <div className='absolute top-0 -z-10 h-24 w-full bg-gray-100/20 backdrop-blur [mask-image:linear-gradient(to_bottom,#000_4.8rem,transparent)]' />
+      <div className='pointer-events-none absolute top-0 -z-10 h-24 w-full select-none bg-gray-100/20 backdrop-blur [mask-image:linear-gradient(to_bottom,#000_4.8rem,transparent)]' />
       <nav className='mx-auto max-w-screen-xl px-8 py-4'>
         <ul className='flex h-10 items-center gap-6'>
           <li>
