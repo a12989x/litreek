d9ce50fdba73b048924a65e24070d2aeb19aac2c
Alexis Guzman <codingcodax@gmail.com>
2023-03-07 17:18:55 -0600

    feat(form): setup form inputs

--- src/components/ui/Form/FormInputs.tsx
+++ src/components/ui/Form/FormInputs.tsx
@@ -1,9 +1,9 @@
-const FormInputs = () => {
-  return (
-    <div>
-      <p>Form Inputs</p>
-    </div>
-  );
+type Props = {
+  children: React.ReactNode;
+};
+
+const FormInputs = ({ children }: Props) => {
+  return <div className='space-y-4'>{children}</div>;
 };
 
 export default FormInputs;
