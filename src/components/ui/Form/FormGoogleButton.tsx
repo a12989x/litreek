9669599fb03005630f7c5693f123f56b33f51121
Alexis Guzman <codingcodax@gmail.com>
2023-03-07 17:55:03 -0600

    feat(form): setup form google button

--- src/components/ui/Form/FormGoogleButton.tsx
+++ src/components/ui/Form/FormGoogleButton.tsx
@@ -1,8 +1,21 @@
-const FormGoogleButton = () => {
+import { signIn } from 'next-auth/react';
+import { GoogleIcon } from '~/components/icons';
+import { Button } from '~/components/ui';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const FormGoogleButton = ({ children }: Props) => {
   return (
-    <div>
-      <p>Form Google Button</p>
-    </div>
+    <Button
+      type='button'
+      onClick={() => signIn('google', { callbackUrl: '/setup' })}
+      intent='ghost'
+      className='w-full space-x-2'
+    >
+      <GoogleIcon className='h-4 w-4' /> <span>{children}</span>
+    </Button>
   );
 };
 
