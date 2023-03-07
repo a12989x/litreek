cd8cde1e7d979c8b00572fddea236735b6480c48
Alexis Guzman <codingcodax@gmail.com>
2023-03-07 17:51:21 -0600

    feat(form): setup form or divider

--- src/components/ui/Form/FormOrDivider.tsx
+++ src/components/ui/Form/FormOrDivider.tsx
@@ -1,7 +1,9 @@
 const FormOrDivider = () => {
   return (
-    <div>
-      <p>Form Or Divider</p>
+    <div className='my-8 grid w-full grid-cols-[1fr_auto_1fr] items-center gap-4'>
+      <div className='h-px bg-gray-600' />
+      <span>Or</span>
+      <div className='h-px bg-gray-600' />
     </div>
   );
 };
