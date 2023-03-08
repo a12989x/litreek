e69b67088e56cb5b750f717f60666adda3977997
Alexis Guzman <codingcodax@gmail.com>
2023-03-07 18:21:26 -0600

    feat(ui): setup form

--- src/components/ui/Form/Form.tsx
+++ src/components/ui/Form/Form.tsx
@@ -1,9 +1,39 @@
-const Form = () => {
+import FormButton from './FormButton';
+import FormContainer from './FormContainer';
+import FormDescription from './FormDescription';
+import FormGitHubButton from './FormGitHubButton';
+import FormGoogleButton from './FormGoogleButton';
+import FormHeader from './FormHeader';
+import FormInput from './FormInput';
+import FormInputWrapper from './FormInputWrapper';
+import FormItems from './FormItems';
+import FormLabel from './FormLabel';
+import FormOrDivider from './FormOrDivider';
+import FormTitle from './FormTitle';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Form = ({ children }: Props) => {
   return (
-    <div>
-      <p>Form</p>
-    </div>
+    <form className='mx-auto w-full max-w-md rounded-lg border border-gray-600 p-6'>
+      {children}
+    </form>
   );
 };
+
+Form.Container = FormContainer;
+Form.Header = FormHeader;
+Form.Title = FormTitle;
+Form.Description = FormDescription;
+Form.Items = FormItems;
+Form.InputWrapper = FormInputWrapper;
+Form.Label = FormLabel;
+Form.Input = FormInput;
+Form.Button = FormButton;
+Form.OrDivider = FormOrDivider;
+Form.GoogleButton = FormGoogleButton;
+Form.GitHubButton = FormGitHubButton;
 
 export default Form;
