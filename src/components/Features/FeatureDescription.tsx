ae7f2683fde583c1b9b1d4e17d3176779d91e0f5
Alexis Guzman <codingcodax@gmail.com>
2023-03-08 10:03:08 -0600

    feat(features): setup feature description

--- src/components/Features/FeatureDescription.tsx
+++ src/components/Features/FeatureDescription.tsx
@@ -1,9 +1,9 @@
-const FeatureDescription = () => {
-  return (
-    <div>
-      <p>Feature Description</p>
-    </div>
-  );
+type Props = {
+  children: React.ReactNode;
+};
+
+const FeatureDescription = ({ children }: Props) => {
+  return <p className='text-center'>{children}</p>;
 };
 
 export default FeatureDescription;
