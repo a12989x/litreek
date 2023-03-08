7e84ca58dcf1d2aff0ca7c10e2e2b845ef9fbfb7
Alexis Guzman <codingcodax@gmail.com>
2023-03-07 18:21:42 -0600

    feat(pages): setup login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,48 @@
 import { type NextPage } from 'next';
+import { Form, Link } from '~/components/ui';
 
 const Login: NextPage = () => {
   return (
-    <div>
-      <p>login page</p>
+    <div className='mx-auto flex w-full max-w-screen-xl items-center justify-center'>
+      <Form>
+        <Form.Container>
+          <Form.Header>
+            <Form.Title>Login</Form.Title>
+            <Form.Description>
+              Don&apos;t have an account? <Link href='/register'>Sign Up</Link>
+            </Form.Description>
+          </Form.Header>
+
+          <Form.Items>
+            <Form.InputWrapper>
+              <Form.Label htmlFor='email'>Email</Form.Label>
+              <Form.Input
+                type='email'
+                id='email'
+                placeholder='name@example.com'
+              />
+            </Form.InputWrapper>
+
+            <Form.InputWrapper>
+              <Form.Label htmlFor='password'>Password</Form.Label>
+              <Form.Input
+                type='password'
+                id='password'
+                placeholder='securepassword'
+              />
+            </Form.InputWrapper>
+          </Form.Items>
+
+          <Form.Button>Log In with Email</Form.Button>
+        </Form.Container>
+
+        <Form.OrDivider />
+
+        <Form.Items>
+          <Form.GoogleButton>Log In with Google</Form.GoogleButton>
+          <Form.GitHubButton>Log In with GitHub</Form.GitHubButton>
+        </Form.Items>
+      </Form>
     </div>
   );
 };
