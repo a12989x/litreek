4b892c45c58286556d5999beec19a49f0a492568
Alexis Guzman <codingcodax@gmail.com>
2023-03-08 11:05:22 -0600

    feat(features): setup and add image, name and description

--- src/components/Features/Feature.tsx
+++ src/components/Features/Feature.tsx
@@ -1,9 +1,17 @@
-const Feature = () => {
-  return (
-    <div>
-      <p>Feature</p>
-    </div>
-  );
+import FeatureDescription from './FeatureDescription';
+import FeatureImage from './FeatureImage';
+import FeatureName from './FeatureName';
+
+type Props = {
+  children: React.ReactNode;
 };
+
+const Feature = ({ children }: Props) => {
+  return <li className='flex flex-col items-center space-y-2'>{children}</li>;
+};
+
+Feature.Image = FeatureImage;
+Feature.Name = FeatureName;
+Feature.Description = FeatureDescription;
 
 export default Feature;
