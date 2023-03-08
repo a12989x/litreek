69ea830b60e7039cec5637c4effd2cbc3066d380
Alexis Guzman <codingcodax@gmail.com>
2023-03-07 19:33:02 -0600

    feat(link): add underline intent variant

--- src/components/ui/Link.tsx
+++ src/components/ui/Link.tsx
@@ -8,7 +8,7 @@ const linkStyles = tv({
   variants: {
     intent: {
       nav: cn(
-        'text-underline inline-flex h-8 items-center justify-center rounded-lg px-3 text-sm font-medium transition-colors',
+        'inline-flex h-8 items-center justify-center rounded-lg px-3 text-sm font-medium transition-colors',
         'hover:bg-indigo-400',
         'active:bg-indigo-500'
       ),
@@ -16,6 +16,7 @@ const linkStyles = tv({
         'font-medium text-gray-900',
         'hover:text-gray-1000 hover:underline'
       ),
+      underline: cn('underline transition-colors', 'hover:text-indigo-900'),
     },
   },
   defaultVariants: {},
