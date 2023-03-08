a31012254afdd536f7fdd38765109e7b7fa49e35
Alexis Guzman <codingcodax@gmail.com>
2023-03-08 12:34:15 -0600

    feat(features): style link in bio text like editable text

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -37,9 +37,13 @@ const features = [
 const Features = () => {
   return (
     <section className='mx-auto my-20 flex max-w-screen-xl flex-col items-center px-8'>
-      <h2 className='mb-4 text-5xl font-bold'>All the features you need.</h2>
+      <h2 className='mb-4 text-5xl font-bold'>All the features you need</h2>
       <p className='mb-8 text-2xl'>
-        Everthing you need to create your perfect link-in-bio
+        Everthing you need to create your perfect{' '}
+        <span className='after:bg-gray-1200 relative font-medium text-gray-100 before:absolute before:-inset-x-px before:top-0 before:-bottom-1 before:-z-10 before:bg-indigo-900 after:absolute after:-top-1 after:-bottom-2 after:right-0 after:w-px'>
+          link-in-bio
+        </span>
+        .
       </p>
       <ul className='grid grid-cols-3 gap-8'>
         {features.map(({ image, name, description }) => (
