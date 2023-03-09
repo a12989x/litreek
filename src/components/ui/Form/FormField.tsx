d007f5c7f82ed206c74a1e4ae91bea6140680e05
Alexis Guzman <codingcodax@gmail.com>
2023-03-09 17:34:07 -0600

    feat(form): use radix form on input wrapper

---
+++ src/components/ui/Form/FormField.tsx
@@ -0,0 +1,18 @@
+import {
+  FormField as RadixFormField,
+  type FormFieldProps,
+} from '@radix-ui/react-form';
+
+interface Props extends FormFieldProps {
+  children: React.ReactNode;
+}
+
+const FormField = ({ children, ...props }: Props) => {
+  return (
+    <RadixFormField className='space-y-2' {...props}>
+      {children}
+    </RadixFormField>
+  );
+};
+
+export default FormField;
--- src/components/ui/Form/FormInputWrapper.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-  children: React.ReactNode;
-};
-
-const FormInputWrapper = ({ children }: Props) => {
-  return <div className='space-y-2'>{children}</div>;
-};
-
-export default FormInputWrapper;
