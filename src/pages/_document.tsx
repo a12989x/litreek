dbc9d43a869463a38acf4524e23a62aa70d9e392
Alexis Guzman <codingcodax@gmail.com>
2023-03-09 09:57:08 -0600

    feat(document): add colors to selection pseudo selector

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ const Document = () => {
   return (
     <Html lang='en' className='scroll-smooth'>
       <Head />
-      <body className='text-gray-1200 bg-gray-100 font-sans antialiased'>
+      <body className='text-gray-1200 bg-gray-100 font-sans antialiased selection:bg-indigo-900 selection:text-gray-100'>
         <Main />
         <NextScript />
       </body>
