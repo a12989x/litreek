22d0d14b5c52e0945c68065370d266d12e6e848f
Alexis Guzman <codingcodax@gmail.com>
2023-03-09 17:33:52 -0600

    feat(form): use radix form on label

--- src/components/ui/Form/FormLabel.tsx
+++ src/components/ui/Form/FormLabel.tsx
@@ -1,17 +1,17 @@
-import { type ComponentProps } from 'react';
-
-type LabelProps = ComponentProps<'label'>;
+import {
+  FormLabel as RadixFormLabel,
+  type LabelProps,
+} from '@radix-ui/react-form';
 
 interface Props extends LabelProps {
-  htmlFor: string;
   children: React.ReactNode;
 }
 
-const FormLabel = ({ htmlFor, children, ...props }: Props) => {
+const FormLabel = ({ children, ...props }: Props) => {
   return (
-    <label htmlFor={htmlFor} className='font-medium' {...props}>
+    <RadixFormLabel className='font-medium' {...props}>
       {children}
-    </label>
+    </RadixFormLabel>
   );
 };
 
