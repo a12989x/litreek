83328c1dcba72e5cd575f7277c830ecbecf7c9e9
Alexis Guzman <codingcodax@gmail.com>
2023-03-09 12:37:16 -0600

    feat(accordion): setup accordion name

--- src/components/ui/Accordion/AccordionName.tsx
+++ src/components/ui/Accordion/AccordionName.tsx
@@ -1,8 +1,22 @@
-const AccordionName = () => {
+import { AccordionHeader, AccordionTrigger } from '@radix-ui/react-accordion';
+import { IconChevronDown } from '@tabler/icons-react';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const AccordionName = ({ children }: Props) => {
   return (
-    <div>
-      <p>Accordion Name</p>
-    </div>
+    <AccordionHeader className='flex border-b border-gray-600'>
+      <AccordionTrigger className='group flex h-10 flex-1 items-center justify-between px-4 font-medium outline-none'>
+        {children}
+
+        <IconChevronDown
+          size={20}
+          className='jtransition-transform ease-in-out group-data-[state=open]:rotate-180'
+        />
+      </AccordionTrigger>
+    </AccordionHeader>
   );
 };
 
