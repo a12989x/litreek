19dfb54607bde5b1a84315a23457621c22adcc77
Alexis Guzman <codingcodax@gmail.com>
2023-03-09 12:37:29 -0600

    feat(accordion): setup accordion description

--- src/components/ui/Accordion/AccordionDescription.tsx
+++ src/components/ui/Accordion/AccordionDescription.tsx
@@ -1,8 +1,14 @@
-const AccordionDescription = () => {
+import { AccordionContent } from '@radix-ui/react-accordion';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const AccordionDescription = ({ children }: Props) => {
   return (
-    <div>
-      <p>Accordion Description</p>
-    </div>
+    <AccordionContent className='data-[state=open]:animate-slide-down data-[state=closed]:animate-slide-up text-gray-1100 overflow-hidden'>
+      <p className='px-4 py-2'>{children}</p>
+    </AccordionContent>
   );
 };
 
