8a66df7d1ded211ad30a3fa3ebf677f1d9dc677a
Alexis Guzman <codingcodax@gmail.com>
2023-03-08 20:10:12 -0600

    feat(pricing): add checked prop to plan feature

--- src/components/Pricing/PlanFeature.tsx
+++ src/components/Pricing/PlanFeature.tsx
@@ -1,13 +1,25 @@
-import { IconCircleCheckFilled } from '@tabler/icons-react';
+import { IconCircleCheckFilled, IconCircleXFilled } from '@tabler/icons-react';
+import { cn } from '~/utils';
 
 type Props = {
+  checked: boolean;
   children: React.ReactNode;
 };
 
-const PlanFeature = ({ children }: Props) => {
+const PlanFeature = ({ checked, children }: Props) => {
   return (
-    <li className='flex items-center gap-x-2'>
-      <IconCircleCheckFilled size={20} /> <span>{children}</span>
+    <li
+      className={cn(
+        'flex items-center gap-x-2',
+        checked ? '' : 'text-gray-1100'
+      )}
+    >
+      {checked ? (
+        <IconCircleCheckFilled size={20} />
+      ) : (
+        <IconCircleXFilled size={20} />
+      )}{' '}
+      <span>{children}</span>
     </li>
   );
 };
