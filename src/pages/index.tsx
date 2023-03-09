a0f221e97eadce1122e681cb6288cdbf067fda52
Alexis Guzman <codingcodax@gmail.com>
2023-03-08 20:10:24 -0600

    feat(home): use pricing component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { type NextPage } from 'next';
-import { Features, Hero } from '~/components';
+import { Features, Hero, Pricing } from '~/components';
 
 const Home: NextPage = () => {
   return (
     <div className='mt-16'>
       <Hero />
       <Features />
+      <Pricing />
     </div>
   );
 };
