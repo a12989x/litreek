f966ba780f2a30851fd908ec19c0342983ce5fd9
Alexis Guzman <codingcodax@gmail.com>
2023-03-09 12:39:29 -0600

    feat(components): setup faqs

--- src/components/Faqs/Faqs.tsx
+++ src/components/Faqs/Faqs.tsx
@@ -1,8 +1,62 @@
+import { Accordion } from '~/components/ui';
+
+const faqs = [
+  {
+    name: 'What are the limits of the Free plan?',
+    description:
+      'The free plan gives you access to the free themes, limited social, block and embed links and 30 days of statics history.',
+  },
+  {
+    name: "What's included in the Prop plan?",
+    description:
+      'The Pro plan gives you access to the paid themes, more social, block and embed links, 90 day of statics history, SEO features and removable branding.',
+  },
+];
+
+const faqs2 = [
+  {
+    name: 'treek.link is secure?',
+    description:
+      "Yes, treek.link don't share your information with anyone, all your information is manageable only by you.",
+  },
+  {
+    name: 'What payments method do you accept?',
+    description: 'We accept credit and debit cards.',
+  },
+];
+
+const defaultValues = faqs.map(({ name }) => name);
+const defaultValues2 = faqs2.map(({ name }) => name);
+
 const Faqs = () => {
   return (
-    <div>
-      <p>Faqs</p>
-    </div>
+    <section
+      id='faq'
+      className='mx-auto flex min-h-[600px] max-w-screen-xl scroll-mt-10 flex-col items-center py-20 px-8'
+    >
+      <h2 className='mb-4 text-5xl font-bold'>Frequently asked questions</h2>
+      <p className='mb-8 text-2xl'>Need answers? Find them here...</p>
+
+      <div className='flex w-full max-w-4xl gap-4'>
+        <Accordion defaultValue={defaultValues}>
+          {faqs.map(({ name, description }) => (
+            <Accordion.Item value={name} key={name}>
+              <Accordion.Name>{name}</Accordion.Name>
+              <Accordion.Description>{description}</Accordion.Description>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+
+        <Accordion defaultValue={defaultValues2}>
+          {faqs2.map(({ name, description }) => (
+            <Accordion.Item value={name} key={name}>
+              <Accordion.Name>{name}</Accordion.Name>
+              <Accordion.Description>{description}</Accordion.Description>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      </div>
+    </section>
   );
 };
 
