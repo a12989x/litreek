df7cf5e0b746992a1e5cee407be5391c3e04cefd
Alexis Guzman <codingcodax@gmail.com>
2023-03-09 12:37:36 -0600

    feat(ui): setup accordion

--- src/components/ui/Accordion/Accordion.tsx
+++ src/components/ui/Accordion/Accordion.tsx
@@ -1,9 +1,29 @@
-const Accordion = () => {
+import { Accordion as RadixAccordion } from '@radix-ui/react-accordion';
+
+import AccordionDescription from './AccordionDescription';
+import AccordionItem from './AccordionItem';
+import AccordionName from './AccordionName';
+
+type Props = {
+  defaultValue?: string[];
+  children: React.ReactNode;
+};
+
+const Accordion = ({ defaultValue, children }: Props) => {
   return (
-    <div>
-      <p>Accordion</p>
-    </div>
+    <RadixAccordion
+      type='multiple'
+      defaultValue={defaultValue}
+      className='flex w-full flex-col gap-4'
+      asChild
+    >
+      <ul>{children}</ul>
+    </RadixAccordion>
   );
 };
+
+Accordion.Item = AccordionItem;
+Accordion.Name = AccordionName;
+Accordion.Description = AccordionDescription;
 
 export default Accordion;
