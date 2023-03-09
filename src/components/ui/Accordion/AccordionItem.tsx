84a0bbfe8022f33757abf5d56e5a81c2829a6e91
Alexis Guzman <codingcodax@gmail.com>
2023-03-09 12:37:05 -0600

    feat(accordion): setup accordion item

--- src/components/ui/Accordion/AccordionItem.tsx
+++ src/components/ui/Accordion/AccordionItem.tsx
@@ -1,8 +1,20 @@
-const AccordionItem = () => {
+import { AccordionItem as RadixAccordionItem } from '@radix-ui/react-accordion';
+
+type Props = {
+  value: string;
+  children: React.ReactNode;
+};
+
+const AccordionItem = ({ value, children }: Props) => {
   return (
-    <div>
-      <p>Accordion Item</p>
-    </div>
+    <RadixAccordionItem
+      value={value}
+      className='w-full max-w-md rounded-lg border border-gray-600 focus-within:ring focus-within:ring-indigo-800'
+      disabled
+      asChild
+    >
+      <li>{children}</li>
+    </RadixAccordionItem>
   );
 };
 
