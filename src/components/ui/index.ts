d18692653d4d1c85d96a6182c1348c1436b46781
Alexis Guzman <codingcodax@gmail.com>
2023-03-09 09:37:16 -0600

    feat(ui): export accordion

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
+export { default as Accordion } from './Accordion';
 export { default as Form } from './Form';
 export { default as Button } from './Button';
 export { default as ButtonOrLink } from './ButtonOrLink';
