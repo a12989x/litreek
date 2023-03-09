1863450261d530948b2ae19747427531f93690de
Alexis Guzman <codingcodax@gmail.com>
2023-03-09 17:49:56 -0600

    feat(form): add field, label wrapper, message and textarea

--- src/components/ui/Form/Form.tsx
+++ src/components/ui/Form/Form.tsx
@@ -1,25 +1,33 @@
+import { Root as RadixForm, type FormProps } from '@radix-ui/react-form';
+
 import FormButton from './FormButton';
 import FormContainer from './FormContainer';
 import FormDescription from './FormDescription';
+import FormField from './FormField';
 import FormGitHubButton from './FormGitHubButton';
 import FormGoogleButton from './FormGoogleButton';
 import FormHeader from './FormHeader';
 import FormInput from './FormInput';
-import FormInputWrapper from './FormInputWrapper';
 import FormItems from './FormItems';
 import FormLabel from './FormLabel';
+import FormLabelWrapper from './FormLabelWrapper';
+import FormMessage from './FormMessage';
 import FormOrDivider from './FormOrDivider';
+import FormTextarea from './FormTextarea';
 import FormTitle from './FormTitle';
 
-type Props = {
+interface Props extends FormProps {
   children: React.ReactNode;
-};
+}
 
-const Form = ({ children }: Props) => {
+const Form = ({ children, ...props }: Props) => {
   return (
-    <form className='mx-auto w-full max-w-md rounded-lg border border-gray-600 p-6'>
+    <RadixForm
+      className='mx-auto w-full max-w-md rounded-lg border border-gray-600 p-6'
+      {...props}
+    >
       {children}
-    </form>
+    </RadixForm>
   );
 };
 
@@ -28,9 +36,12 @@ Form.Header = FormHeader;
 Form.Title = FormTitle;
 Form.Description = FormDescription;
 Form.Items = FormItems;
-Form.InputWrapper = FormInputWrapper;
+Form.Field = FormField;
+Form.LabelWrapper = FormLabelWrapper;
 Form.Label = FormLabel;
+Form.Message = FormMessage;
 Form.Input = FormInput;
+Form.Textarea = FormTextarea;
 Form.Button = FormButton;
 Form.OrDivider = FormOrDivider;
 Form.GoogleButton = FormGoogleButton;
