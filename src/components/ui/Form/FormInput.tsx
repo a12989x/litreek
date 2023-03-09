9647c03c2b0bfea6bacf2f3c2dab3de7cfa8191b
Alexis Guzman <codingcodax@gmail.com>
2023-03-09 17:33:36 -0600

    feat(form): use radix form on input

--- src/components/ui/Form/FormInput.tsx
+++ src/components/ui/Form/FormInput.tsx
@@ -1,21 +1,22 @@
+import { FormControl } from '@radix-ui/react-form';
 import { Input } from '~/components/ui';
 import { type Props as InputProps } from '~/components/ui/Input';
 
 interface Props extends InputProps {
   type: string;
-  id: string;
   placeholder: string;
 }
 
-const FormInput = ({ type, id, placeholder, ...props }: Props) => {
+const FormInput = ({ type, placeholder, ...props }: Props) => {
   return (
-    <Input
-      type={type}
-      id={id}
-      placeholder={placeholder}
-      className='w-full bg-gray-100'
-      {...props}
-    />
+    <FormControl asChild>
+      <Input
+        type={type}
+        placeholder={placeholder}
+        className='w-full bg-gray-100'
+        {...props}
+      />
+    </FormControl>
   );
 };
 
