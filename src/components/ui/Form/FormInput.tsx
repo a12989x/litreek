dd585b8753733623bdca85d5ff7d3641b69a89ed
Alexis Guzman <codingcodax@gmail.com>
2023-03-10 10:14:01 -0600

    feat(form): add styles when invalid for form input

--- src/components/ui/Form/FormInput.tsx
+++ src/components/ui/Form/FormInput.tsx
@@ -1,6 +1,7 @@
 import { FormControl } from '@radix-ui/react-form';
 import { Input } from '~/components/ui';
 import { type Props as InputProps } from '~/components/ui/Input';
+import { cn } from '~/utils';
 
 interface Props extends InputProps {
   type: string;
@@ -13,7 +14,11 @@ const FormInput = ({ type, placeholder, ...props }: Props) => {
       <Input
         type={type}
         placeholder={placeholder}
-        className='w-full bg-gray-100'
+        className={cn(
+          'w-full bg-gray-100',
+          'data-[invalid=true]:border-red-700 data-[invalid=true]:outline-red-800',
+          'hover:data-[invalid=true]:border-red-800'
+        )}
         {...props}
       />
     </FormControl>
