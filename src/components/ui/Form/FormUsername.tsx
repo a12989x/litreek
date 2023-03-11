c190e23d6e5d566171ef92f7bc4e7319588f8d67
Alexis Guzman <codingcodax@gmail.com>
2023-03-10 20:56:40 -0600

    feat(form): setup username form

--- src/components/ui/Form/FormUsername.tsx
+++ src/components/ui/Form/FormUsername.tsx
@@ -1,7 +1,28 @@
-const FormUsername = () => {
+import { Label } from '@radix-ui/react-label';
+import { cn } from '~/utils';
+
+import { type Props as InputProps } from '~/components/ui/Input';
+
+import FormInput from './FormInput';
+
+type Props = InputProps;
+const FormUsername = ({}: Props) => {
   return (
-    <div>
-      <p>Form Username</p>
+    <div className='relative w-full'>
+      <Label
+        htmlFor='username'
+        className='absolute left-0 flex h-10 items-center pl-4 font-medium'
+      >
+        treek.link/
+      </Label>
+      <FormInput
+        type='text'
+        id='username'
+        placeholder='codingcodax'
+        className={cn('w-full pl-[92.3167px]')}
+        minLength={4}
+        required
+      />
     </div>
   );
 };
