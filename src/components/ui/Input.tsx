3eac3fc2c0ef997094d5852ce1d668cf788c5610
Alexis Guzman <codingcodax@gmail.com>
2023-03-12 13:38:02 -0600

    feat(input): add disabled style

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -8,6 +8,7 @@ export const inputStyles = tv({
   base: cn(
     'h-10 rounded-lg border border-gray-700 px-4 transition-colors',
     'hover:border-gray-800',
+    'disabled:bg-gray-200',
     'outline-indigo-800 focus:outline',
     'placeholder:text-gray-800'
   ),
@@ -17,7 +18,7 @@ export const inputStyles = tv({
 
 type InputVariants = VariantProps<typeof inputStyles>;
 
-export interface Props extends InputProps, InputVariants { }
+export interface Props extends InputProps, InputVariants {}
 
 const Input = forwardRef<HTMLInputElement, Props>(
   ({ className, ...props }, ref) => {
