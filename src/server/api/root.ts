38af7280a75d38287fe1fac52103f72284afb464
Alexis Guzman <codingcodax@gmail.com>
2023-03-13 14:17:31 -0600

    feat(api): add social router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { userRouter } from '~/server/api/routers';
+import { socialRouter, userRouter } from '~/server/api/routers';
 import { createTRPCRouter } from '~/server/api/trpc';
 
 /**
@@ -7,6 +7,7 @@ import { createTRPCRouter } from '~/server/api/trpc';
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  social: socialRouter,
   user: userRouter,
 });
 
