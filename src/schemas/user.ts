f5e8679dca9d5baacb37303107814caa9d041326
Alexis Guzman <codingcodax@gmail.com>
2023-03-14 10:32:32 -0600

    feat(schemas): create user and extend to update

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,8 +1,10 @@
 import { z } from 'zod';
 
-export const updateUserSchema = z.object({
+export const userSchema = z.object({
   name: z.string(),
   username: z.string(),
   bio: z.string().optional(),
   location: z.string().optional(),
 });
+
+export const updateUserSchema = userSchema.extend({});
