c773af035b1dc29b936145c50ec10d9f5baed9ef
Alexis Guzman <codingcodax@gmail.com>
2023-03-17 20:40:52 -0600

    feat(header): user dropdown menu account

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,16 @@
 import { useRouter } from 'next/router';
-import {
-  IconChevronDown,
-  IconCircleDot,
-  IconEdit,
-  IconLogout,
-  IconSettings,
-  IconUser,
-} from '@tabler/icons-react';
-import { signOut } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import { cn } from '~/utils';
 
-import { Button, buttonStyles } from '~/components/ui/Button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '~/components/ui/DropdownMenu';
+import { buttonStyles } from '~/components/ui/Button';
 import { Link } from '~/components/ui/Link';
 
+import DropdownMenuAccount from './DropdownMenuAccount';
 import NavLink from './NavLink';
 
 const Header = () => {
   const { asPath } = useRouter();
+  const { data: sessionData } = useSession();
   const isDashboard = asPath.includes('/dashboard');
 
   return (
@@ -42,7 +27,15 @@ const Header = () => {
               <NavLink href='/#features'>Features</NavLink>
               <NavLink href='/#pricing'>Pricing</NavLink>
               <NavLink href='/#faq'>FAQ</NavLink>
+            </>
+          )}
 
+          {sessionData?.user ? (
+            <li className='!ml-auto'>
+              <DropdownMenuAccount />
+            </li>
+          ) : (
+            <>
               <li className='!ml-auto'>
                 <Link href='/login' className='hover:bg-gray-400' intent='nav'>
                   Login
@@ -60,66 +53,6 @@ const Header = () => {
                 </Link>
               </li>
             </>
-          )}
-
-          {isDashboard && (
-            <li className='!ml-auto'>
-              <DropdownMenu>
-                <DropdownMenuTrigger className='group' asChild>
-                  <Button>
-                    User{' '}
-                    <IconChevronDown
-                      className='ml-2 transition-transform ease-in-out group-data-[state=open]:rotate-180'
-                      size={20}
-                    />
-                  </Button>
-                </DropdownMenuTrigger>
-
-                <DropdownMenuContent align='end' className='w-56'>
-                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-
-                  <DropdownMenuGroup>
-                    <DropdownMenuItem asChild>
-                      <Link href='/dashboard'>
-                        <IconEdit className='mr-2' size={20} /> Manage links
-                      </Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem asChild>
-                      <Link isExternal href='/'>
-                        <IconUser className='mr-2' size={20} />
-                        View page
-                      </Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem asChild>
-                      <Link href='/account'>
-                        <IconSettings className='mr-2' size={20} /> Account
-                        settings
-                      </Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem asChild>
-                      <Link
-                        isExternal
-                        href='https://github.com/codingcodax/litreek/issues/new'
-                      >
-                        <IconCircleDot className='mr-2' size={20} />
-                        Report a bug
-                      </Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      className='w-full text-red-900 data-[highlighted]:bg-red-400'
-                      asChild
-                    >
-                      <button onClick={() => signOut({ callbackUrl: '/' })}>
-                        <IconLogout className='mr-2' size={20} />
-                        Sign out
-                      </button>
-                    </DropdownMenuItem>
-                  </DropdownMenuGroup>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </li>
           )}
         </ul>
       </nav>
