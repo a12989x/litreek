ff47419d02fe4f7f7df706890eacc63bbb050272
Alexis Guzman <codingcodax@gmail.com>
2023-03-27 14:21:38 -0600

    feat(dashlayout): setup tab link

--- src/components/DashboardLayout/Tabs/TabLink.tsx
+++ src/components/DashboardLayout/Tabs/TabLink.tsx
@@ -1,8 +1,28 @@
-const TabLink = () => {
+import { useRouter } from 'next/router';
+import { cn } from '~/utils';
+
+import { Link } from '~/components/ui/Link';
+
+type Props = {
+  href: string;
+  children: React.ReactNode;
+};
+
+const TabLink = ({ href, children }: Props) => {
+  const { asPath } = useRouter();
+
   return (
-    <div>
-      <p>TabLink</p>
-    </div>
+    <li>
+      <Link
+        className={cn(
+          href === asPath ? 'bg-indigo-500 hover:bg-indigo-500' : ''
+        )}
+        href={href}
+        intent='nav'
+      >
+        {children}
+      </Link>
+    </li>
   );
 };
 
