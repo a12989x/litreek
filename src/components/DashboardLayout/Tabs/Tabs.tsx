b7dc294ce84e46a73495f5abd3ae47d71807c25e
Alexis Guzman <codingcodax@gmail.com>
2023-03-27 14:21:53 -0600

    feat(dashlayout): setup tabs

--- src/components/DashboardLayout/Tabs/Tabs.tsx
+++ src/components/DashboardLayout/Tabs/Tabs.tsx
@@ -1,8 +1,15 @@
+import TabLink from './TabLink';
+
 const Tabs = () => {
   return (
-    <div>
-      <p>Tabs</p>
-    </div>
+    <nav className='border-b border-b-gray-600'>
+      <ul className='flex space-x-6 pb-2'>
+        <TabLink href='/dashboard/links'>Link</TabLink>
+        <TabLink href='/dashboard/design'>Design</TabLink>
+        <TabLink href='/dashboard/stats'>Stats</TabLink>
+        <TabLink href='/dashboard/settings'>Settings</TabLink>
+      </ul>
+    </nav>
   );
 };
 
