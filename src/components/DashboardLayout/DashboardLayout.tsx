e5bb71a8b5edb4066a38222b41305b11fadced57
Alexis Guzman <codingcodax@gmail.com>
2023-03-27 14:24:11 -0600

    feat(components): setup dashboard layout

--- src/components/DashboardLayout/DashboardLayout.tsx
+++ src/components/DashboardLayout/DashboardLayout.tsx
@@ -1,7 +1,19 @@
-const DashboardLayout = () => {
+import Tabs from './Tabs/Tabs';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const DashboardLayout = ({ children }: Props) => {
   return (
-    <div>
-      <p>DashboardLayout</p>
+    <div className='relative mx-auto grid w-full max-w-screen-xl grid-cols-3 px-8'>
+      <div className='col-span-2'>
+        <Tabs />
+
+        <div className='mt-6'>{children}</div>
+      </div>
+
+      <div>Preview</div>
     </div>
   );
 };
